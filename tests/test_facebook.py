db47a21dd65380700620ccbd4c6c12ce0ff87e91
itssafi <jsafiulla@zeomega.com>
2015-03-02 11:50:45 +0530

    Starting write test cases

---
+++ tests/test_facebook.py
@@ -0,0 +1,10 @@
+frm unittest import unittest
+
+class Test(unittest.TestCase):
+    """
+    Test Cases for facebook application
+    """
+    def test_send_friend_request(self):
+        """
+        """
+        pass
